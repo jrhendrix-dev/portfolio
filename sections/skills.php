57c44cf4dc14ac91db8181ebd59e9f1ecdf84435
Jonathan Ray Hendrix López <jrhendrixdev@gmail.com>
2025-10-11 04:10:06 +0200

    Updated skills section in both languages

--- sections/skills.php
+++ sections/skills.php
@@ -5,27 +5,35 @@
         <div class="skills-column">
             <h3><i class="fas fa-code" aria-hidden="true"></i> Frontend</h3>
             <ul>
-                <li><i class="fab fa-html5" aria-hidden="true"></i> <strong>HTML5 &amp; CSS3</strong></li>
-                <li><i class="fab fa-js-square" aria-hidden="true"></i> JavaScript (Vanilla), jQuery</li>
+                <li><i class="fab fa-html5" aria-hidden="true"></i> <strong>HTML5 & CSS3</strong></li>
+                <li><i class="fa-brands fa-css" aria-hidden="true"></i> Tailwind CSS</li>
+                <li><i class="fab fa-js-square" aria-hidden="true"></i> JavaScript, jQuery, Typescript</li>
                 <li><i class="fab fa-bootstrap" aria-hidden="true"></i> Bootstrap</li>
+                <li><i class="fab fa-angular" aria-hidden="true"></i> Angular</li>
                 <li><i class="fab fa-react" aria-hidden="true"></i> React (Basics)</li>
             </ul>
         </div>
         <div class="skills-column">
             <h3><i class="fas fa-server" aria-hidden="true"></i> Backend</h3>
             <ul>
-                <li><i class="fab fa-php" aria-hidden="true"></i> PHP (OOP &amp; Procedural)</li>
-                <li><i class="fab fa-java" aria-hidden="true"></i> Java (Spring Boot, Hibernate)</li>
-                <li><i class="fas fa-database" aria-hidden="true"></i> MySQL / SQL</li>
+                <li><i class="fab fa-php" aria-hidden="true"></i> PHP (OOP & Procedural)</li>
+                <li><i class="fab fa-symfony" aria-hidden="true"></i> Symfony </li>
+                <li><i class="fab fa-java" aria-hidden="true"></i> Java</li>
+                <li><i class="fa-solid fa-leaf" aria-hidden="true"></i>Spring Boot, Hibernate</li>
+                <li><i class="fas fa-database" aria-hidden="true"></i> MySQL, SQL, PostgreSQL</li>
             </ul>
         </div>
         <div class="skills-column">
-            <h3><i class="fas fa-tools" aria-hidden="true"></i> Tools &amp; Other</h3>
+            <h3><i class="fas fa-tools" aria-hidden="true"></i> Tools & Other</h3>
             <ul>
-                <li><i class="fab fa-git-alt" aria-hidden="true"></i> Git &amp; GitHub</li>
+                <li><i class="fab fa-git-alt" aria-hidden="true"></i> Git & GitHub</li>
                 <li><i class="fas fa-laptop-code" aria-hidden="true"></i> IntelliJ, VS Code</li>
-                <li><i class="fas fa-project-diagram" aria-hidden="true"></i> Agile &amp; Scrum Basics</li>
-                <li><i class="fas fa-language" aria-hidden="true"></i> Bilingual: English &amp; Spanish</li>
+                <li><i class="fas fa-project-diagram" aria-hidden="true"></i> Agile, Scrum, Kanban</li>
+                <li><i class="fas fa-language" aria-hidden="true"></i> Bilingual: English & Spanish</li>
+                <li><i class="fa-solid fa-user-astronaut" aria-hidden="true"></i> Postman</li>
+                <li><i class="fa-brands fa-docker" aria-hidden="true"></i> Docker</li>
+                <li><i class="fa-brands fa-ubuntu" aria-hidden="true"></i> Linux Ubuntu</li>
+                <li><i class="fa-solid fa-m" aria-hidden="true"></i> Mailtrap</li>
             </ul>
         </div>
     </div>
